36f1ab91a1b9e74c87618f227a7f1921be0ebcbc
AnnaNik334743 <annanikiforova564@gmail.com>
2024-11-09 21:58:25 +0300

    а э ы

--- app/backend/files_processing.py
+++ app/backend/files_processing.py
@@ -1,9 +1,7 @@
-from utils import detect_language, get_file_extension
-from retrieval import init_all as retrieval_init_all
+from utils import get_file_extension
 from transformers import AutoProcessor, PaliGemmaForConditionalGeneration
 from easyocr import Reader
 import os
-import shutil
 import time
 import torch
 import csv 
@@ -12,6 +10,7 @@ from PIL import Image
 import nltk
 from nltk.tokenize import word_tokenize
 import fitz
+import pandas as pd
 
 nltk.download('punkt', quiet=True)
 
@@ -22,26 +21,10 @@ CHART2TEXT = None
 
 
 class OCRModel:
-    """
-    OCR class for extracting text from images using EasyOCR.
-
-    Methods:
-        extract_text(image): Extracts text from a given image.
-    """
-
     def __init__(self):
-        self.reader = easyocr.Reader(['en', 'ru'], gpu=torch.cuda.is_available())
+        self.reader = Reader(['en', 'ru'], gpu=torch.cuda.is_available())
 
     def extract_text(self, image: Image.Image) -> str:
-        """
-        Extracts text from a given image using EasyOCR.
-
-        Parameters:
-            image (Image.Image): The image to extract text from.
-
-        Returns:
-            str: The extracted text.
-        """
         image_np = np.array(image)
         results = self.reader.readtext(image_np)
         text = ' '.join([result[1] for result in results])
@@ -49,13 +32,6 @@ class OCRModel:
 
 
 class Chart2Text:
-    """
-    CHART2TEXT class for extracting text descriptions from chart images.
-
-    Methods:
-        extract_text(image): Extracts descriptive text from a chart image.
-    """
-
     def __init__(self):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = PaliGemmaForConditionalGeneration.from_pretrained("ahmed-masry/chartgemma",
@@ -63,15 +39,6 @@ class Chart2Text:
         self.processor = AutoProcessor.from_pretrained("ahmed-masry/chartgemma")
 
     def extract_text(self, image: Image.Image) -> str:
-        """
-        Extracts descriptive text from a chart image using the Chart2Text model.
-
-        Parameters:
-            image (Image.Image): The chart image to process.
-
-        Returns:
-            str: The extracted descriptive text.
-        """
         input_text = "program of thought: describe the chart in great detail"
         inputs = self.processor(text=input_text, images=image, return_tensors="pt").to(self.device)
         prompt_length = inputs['input_ids'].shape[1]
@@ -88,33 +55,27 @@ def init_all():
     CHART2TEXT = Chart2Text()
 
 
-def split_text_into_chunks(text: str, chunk_size: int = 250, overlap: int = 10):
+def get_text_pieces_from_txt_file(
+        filepath: str,
+        chunk_size: int = 250,
+        overlap: int = 10
+):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        text = f.read()
     tokens = word_tokenize(text)
     total_tokens = len(tokens)
     chunks = []
     start = 0
     while start < total_tokens:
-        end = min(start + chunk_size, total_tokens)
-        chunk_tokens = tokens[start:end]
+        chunk_tokens = tokens[start:min(start + chunk_size, total_tokens)]
         chunks.append(" ".join(chunk_tokens))
         start += chunk_size - overlap
     return chunks
 
 
-def get_text_pieces_from_txt_file(
-        filepath: str,
-        chunk_size: int = 250,
-        overlap: int = 15
-):
-    with open(filepath, 'r', encoding='utf-8') as f:
-        text = f.read()
-    chunks = split_text_into_chunks(text, chunk_size, overlap)
-    return chunks
-
-
 def get_text_pieces_from_pdf_file(
         filepath: str,
-        too_few_chars: int = 100
+        too_few_chars: int = 50
 ):
     global OCR, CHART2TEXT
 
@@ -136,18 +97,11 @@ def get_text_pieces_from_pdf_file(
 
 def write_data_to_csv(data):
     global CSV_FILE_PATH
-    """
-    Writes data to the global CSV file. Handles concurrent access by using a lock.
-
-    Parameters:
-        data (CSVRow): The data to write to the CSV file.
-    """
     while True:
         try:
             file_exists = os.path.isfile(CSV_FILE_PATH)
             with open(CSV_FILE_PATH, mode='a', newline='', encoding='utf-8') as csvfile:
-                fieldnames = ['filename', 'n_slide', 'text']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer = csv.DictWriter(csvfile, fieldnames=['filename', 'n_slide', 'text'])
                 if not file_exists:
                     writer.writeheader()
                 writer.writerow({
@@ -157,7 +111,6 @@ def write_data_to_csv(data):
                 })
             break
         except IOError:
-            # Wait and retry if the file is being accessed by another process
             time.sleep(1)
             continue
             
@@ -166,30 +119,17 @@ def upload_filedata_to_csv_file(
         filepath: str,
         too_few_chars: int = 50,
         chunk_size: int = 250,
-        overlap: int = 15,
+        overlap: int = 10,
 ):
     filename = os.path.basename(filepath)
     extension = get_file_extension(filename)
-
     if extension == '.txt':
         text_pieces = get_text_pieces_from_txt_file(filepath, chunk_size, overlap)
-        for idx, text_piece in enumerate(text_pieces):
-            csv_row = {'filename': filename, 'n_slide': None, 'text': text_piece}
-            write_data_to_csv(csv_row)
-        
     elif extension == '.pdf':
         text_pieces = get_text_pieces_from_pdf_file(filepath, too_few_chars)
-        for idx, text_piece in enumerate(text_pieces):
-            csv_row = {'filename': filename, 'n_slide': idx+1, 'text': text_piece}
-            write_data_to_csv(csv_row)
     else:
         raise ValueError('Unsupported file extension')
-
-
-def index_filepaths(filepaths):
-    for filepath in filepaths:
-        upload_filedata_to_csv_file(filepath)
-    
-    shutil.rmtree('index')  # create new index
-    retrieval_init_all(save_index_path='index.faiss')
-    
+    for idx, text_piece in enumerate(text_pieces):
+            csv_row = {'filename': filename, 'n_slide': None, 'text': text_piece}
+            write_data_to_csv(csv_row)
+            
--- app/backend/retrieval.py
+++ app/backend/retrieval.py
@@ -7,29 +7,50 @@ import faiss
 DATA = None
 EMBEDDER = None
 INDEX = None
+EMBEDDINGS = None 
 
 
-def init_all(data_path='app/backend/indexed_data.csv', embedder_name='sergeyzh/rubert-tiny-turbo', index_path=None, save_index_path=None):
-    global DATA, EMBEDDER, INDEX
+def init_all(data_path='app/backend/indexed_data.csv', embedder_name='sergeyzh/rubert-tiny-turbo', index_path='app/backend/index.faiss', embeddings_path='app/backend/embeddings.npz',
+             save_index_path='app/backend/index.faiss', save_embeddings_path='app/backend/embeddings.npz'):
+    global DATA, EMBEDDER, INDEX, EMBEDDINGS
     DATA = pd.read_csv(data_path)
     EMBEDDER = SentenceTransformer(embedder_name)
-    if index_path is None:
-        embeddings = EMBEDDER.encode(DATA['text'].values, convert_to_numpy=True, batch_size=16)
-        INDEX = faiss.IndexFlatIP(embeddings.shape[1])
-        embeddings = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
-        INDEX.add(embeddings.astype(np.float32))
+    
+    if (INDEX is None or EMBEDDINGS is None) and index_path and embeddings_path:
+        INDEX = faiss.read_index(index_path)
+        EMBEDDINGS = np.load(embeddings_path)['embeddings']
+    else:
+        EMBEDDINGS = EMBEDDER.encode(DATA['text'].values, convert_to_numpy=True, batch_size=16)
+        EMBEDDINGS = EMBEDDINGS / np.linalg.norm(EMBEDDINGS, axis=1, keepdims=True)
+        INDEX = faiss.IndexFlatIP(EMBEDDINGS.shape[1])
+        INDEX.add(EMBEDDINGS.astype(np.float32))
+        
         if save_index_path:
             faiss.write_index(INDEX, save_index_path)
-    else:
-        INDEX = faiss.read_index(index_path)
-        
+        if save_embeddings_path:
+            np.savez(save_embeddings_path, embeddings=EMBEDDINGS)
+            
 
 def find_similar_neighbors(query, k=3):
     global DATA, EMBEDDER, INDEX
-    if DATA is None or EMBEDDER is None or INDEX is None:
-        init_all()
     query_vector = EMBEDDER.encode([query]).flatten()
     query_vector /= np.linalg.norm(query_vector)
     distances, neighbors = INDEX.search(np.array([query_vector], dtype=np.float32), k)
     similar_neighbors = [DATA.iloc[idx] for idx in neighbors.flatten()]
     return similar_neighbors
+
+
+def index_new_data(new_data_csv_filepath='app/backend/indexed_data.csv', save_index_path='app/backend/index.faiss', save_embeddings_path='app/backend/embeddings.npz'):
+    global DATA, EMBEDDER, INDEX, EMBEDDINGS
+    
+    new_data = pd.read_csv(new_data_csv_filepath)
+    new_texts = new_data['text'].values
+    new_embeddings = EMBEDDER.encode(new_texts, convert_to_numpy=True, batch_size=16)
+    new_embeddings = new_embeddings / np.linalg.norm(new_embeddings, axis=1, keepdims=True)
+    
+    INDEX.add(new_embeddings.astype(np.float32))
+    faiss.write_index(INDEX, save_index_path)
+    
+    EMBEDDINGS = np.vstack((EMBEDDINGS, new_embeddings)) 
+    np.savez(save_embeddings_path, embeddings=EMBEDDINGS)
+     
